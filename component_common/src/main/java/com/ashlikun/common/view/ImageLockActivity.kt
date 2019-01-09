cb99d51b78dcf199c2ebd2492322ccfbdb883c0b
likun <496546144@qq.com>
2019-01-09 22:41:00 +0800

    优化

--- component_common/src/main/java/com/ashlikun/common/view/ImageLockActivity.kt
+++ component_common/src/main/java/com/ashlikun/common/view/ImageLockActivity.kt
@@ -2,20 +2,29 @@ package com.ashlikun.common.view
 
 import android.content.Context
 import android.support.v4.view.ViewPager
+import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import com.alibaba.android.arouter.facade.annotation.Autowired
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.ashlikun.baseproject.libcore.constant.RouterKey
 import com.ashlikun.baseproject.libcore.constant.RouterPath
+import com.ashlikun.circleprogress.CircleProgressView
 import com.ashlikun.common.R
 import com.ashlikun.common.mode.javabean.ImageData
 import com.ashlikun.core.activity.BaseActivity
-import com.ashlikun.glideutils.GlideUtils
+import com.ashlikun.glideutils.GlideLoad
 import com.ashlikun.photoview.PhotoView
 import com.ashlikun.photoview.ScaleFinishView
+import com.ashlikun.utils.other.DimensUtils
+import com.ashlikun.utils.ui.UiUtils
 import com.ashlikun.xviewpager.listener.ViewPageHelperListener
 import com.ashlikun.xviewpager.view.BannerViewPager
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.activity_image_lock.*
 import java.util.*
 
@@ -55,10 +64,28 @@ class ImageLockActivity : BaseActivity(), ViewPageHelperListener<ImageData>, Sca
         val scaleFinishView = ScaleFinishView(this)
         scaleFinishView.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
         scaleFinishView.setOnSwipeListener(this)
+
+        val progressView = UiUtils.getInflaterView(this, R.layout.view_circle_loadding) as CircleProgressView
         val photoView = PhotoView(this)
         scaleFinishView.addView(photoView, ViewGroup.LayoutParams(-1, -1))
-        GlideUtils.show(photoView, data.img_url)
+        GlideLoad.with(this)
+                .load(data.image)
+                .requestListener(object : RequestListener<Any> {
+                    override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Any>?, isFirstResource: Boolean): Boolean {
+                        progressView.visibility = View.GONE
+                        return false
+                    }
+
+                    override fun onResourceReady(resource: Any?, model: Any?, target: Target<Any>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                        progressView.visibility = View.GONE
+                        return false
+                    }
+                })
+                .show(photoView)
         photoView.setOnClickListener(this)
+        val params = FrameLayout.LayoutParams(DimensUtils.dip2px(this, 48f), DimensUtils.dip2px(this, 48f))
+        params.gravity = Gravity.CENTER
+        scaleFinishView.addView(progressView, params)
         return scaleFinishView
     }
 
