8c6d98e9926a5b5f2534fc50b23ad69b317715be
likun <496546144@qq.com>
2019-03-05 17:54:31 +0800

    优化

--- component_common/src/main/java/com/ashlikun/common/adapter/MultipleAdapterHelp.kt
+++ component_common/src/main/java/com/ashlikun/common/adapter/MultipleAdapterHelp.kt
@@ -1,32 +1,52 @@
 package com.ashlikun.common.adapter
 
+import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.LifecycleOwner
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 
 import com.alibaba.android.vlayout.VirtualLayoutManager
 import com.ashlikun.adapter.recyclerview.vlayout.MultipleAdapter
+import com.ashlikun.utils.main.ActivityUtils
 
 /**
  * 作者　　: 李坤
  * 创建时间: 2018/5/29 0029　上午 10:26
  * 邮箱　　：496546144@qq.com
- * @param hasConsistItemType 子适配器项类型是否一致,可以复用相同的viewtype
+ *
  *
  * 功能介绍：MultipleAdapter 的帮助类
  */
-abstract class MultipleAdapterHelp @JvmOverloads constructor(context: Context, recyclerView: RecyclerView, hasConsistItemType: Boolean = true) {
+open class MultipleAdapterHelp
+/**
+ * @param hasConsistItemType 子适配器项类型是否一致,可以复用相同的viewtype
+ */
+@JvmOverloads constructor(context: Context, protected var recyclerView: RecyclerView, hasConsistItemType: Boolean = true) {
     var adapter: MultipleAdapter
-        private set
+        protected set
     var context: Context
-        private set
+        protected set
+    var lifecycle: Lifecycle? = null
+    var layoutManager: VirtualLayoutManager
+        protected set
 
     init {
         this.context = context
-        val layoutManager = VirtualLayoutManager(context)
+        if (context is LifecycleOwner) {
+            lifecycle = (context as LifecycleOwner).lifecycle
+        } else {
+            val activity = ActivityUtils.getActivity(context)
+            if (activity is LifecycleOwner) {
+                lifecycle = (activity as LifecycleOwner).lifecycle
+            }
+        }
+
+        layoutManager = VirtualLayoutManager(context)
         this.adapter = MultipleAdapter(layoutManager, hasConsistItemType)
         recyclerView.layoutManager = layoutManager
         recyclerView.adapter = adapter
     }
+
 
     /**
      * 清空数据
