f851cd9ae22cae0e0aac9b42203104bccc6f5946
李坤 <496546144@qq.com>
2021-09-18 20:28:40 +0800

    1

--- component_core/src/main/java/com/ashlikun/baseproject/libcore/mode/ApiBase.kt
+++ component_core/src/main/java/com/ashlikun/baseproject/libcore/mode/ApiBase.kt
@@ -11,10 +11,16 @@ import java.io.Serializable
  * 创建时间: 2021/5/12　15:19
  * 邮箱　　：496546144@qq.com
  *
- * 功能介绍：
+ * 功能介绍：必须有个方法或者变量，不然正式包的时候会丢失
  */
-@Url(url = HttpManager.URL_PROD, method = "POST")
+@Url(url = HttpManager.URL_PROD, method = ApiBase.method)
 @Path(HttpManager.BASE_PATH)
 @Action(HttpManager.ACTION)
 interface ApiBase : Serializable {
+    /**
+     * 必须有个，不然打正式包的时候会丢失
+     */
+    companion object {
+        const val method = "POST"
+    }
 }
