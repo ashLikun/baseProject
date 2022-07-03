9b95f3e27e352b6147a3d59241284f0258b838f9
爱生活、李坤 <496546144@qq.com>
2022-07-03 23:31:15 +0800

    优化

--- component_core/src/main/java/com/ashlikun/baseproject/libcore/dialog/LoadTransDialog.kt
+++ component_core/src/main/java/com/ashlikun/baseproject/libcore/dialog/LoadTransDialog.kt
@@ -1,9 +1,13 @@
 package com.ashlikun.baseproject.libcore.dialog
 
 import android.content.Context
+import android.graphics.drawable.GradientDrawable
 import android.view.WindowManager
 import com.ashlikun.baseproject.libcore.R
+import com.ashlikun.customdialog.DialogUtils
 import com.ashlikun.customdialog.LoadDialog
+import com.ashlikun.utils.ui.extend.dp
+import com.ashlikun.utils.ui.extend.resColor
 
 /**
  * 作者　　: 李坤
@@ -13,6 +17,12 @@ import com.ashlikun.customdialog.LoadDialog
  * 功能介绍：背景透明，只有红圈
  */
 class LoadTransDialog(context: Context) : LoadDialog(context) {
+    override val backgroundDrawable by lazy {
+        GradientDrawable().apply {
+            setColor(R.color.translucent.resColor)
+            cornerRadius = 10.dp.toFloat()
+        }
+    }
 
     override fun initWindowParams(params: WindowManager.LayoutParams) {
         super.initWindowParams(params)
