fc3384fd4f54eb44de96f31b828a9f22351372fd
likun <496546144@qq.com>
2022-07-12 17:09:10 +0800

    优化

---
+++ component_common/src/main/java/com/ashlikun/baseproject/common/widget/anim/AddImageItemAnimator.kt
@@ -0,0 +1,34 @@
+package com.vinka.ebike.common.widget.anim
+
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * 作者　　: 李坤
+ * 创建时间: 2019/3/21　17:18
+ * 邮箱　　：496546144@qq.com
+ *
+ * 功能介绍：去除透明度动画
+ */
+class AddImageItemAnimator : DefaultItemAnimator() {
+
+    override fun animateChange(
+        oldHolder: RecyclerView.ViewHolder?,
+        newHolder: RecyclerView.ViewHolder?,
+        fromX: Int,
+        fromY: Int,
+        toX: Int,
+        toY: Int
+    ): Boolean {
+        var boolean = super.animateChange(oldHolder, newHolder, fromX, fromY, toX, toY)
+        newHolder?.itemView?.alpha = 1f
+        return boolean
+    }
+
+    override fun animateAdd(holder: RecyclerView.ViewHolder?): Boolean {
+        var boolean = super.animateAdd(holder)
+        holder?.itemView?.alpha = 1f
+        return boolean
+
+    }
+}
